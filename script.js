0a1dabec6d2c24bc5ba008a7c249e4590055d344
Dajmen Graus <2062141graus@zuyd.nl>
2022-04-12 10:53:10 +0200

    super cool front end stuff :)

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 $(document).ready(function(){
     $(window).scroll(function(){
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-    })
-
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
     // toggle menu/navbar script
 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Explainable AI", "Reinforcement Learning"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Reinforcement Learning?"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // owl carousel script
@@ -37,22 +69,7 @@ $(document).ready(function(){
     });
 });
 
-function home(){document.querySelector('#home').scrollIntoView({
-    behavior: 'smooth'
-  });}
-
-function about() {document.querySelector('#about').scrollIntoView({
-    behavior: 'smooth'
-  });}
-
-function services(){document.querySelector('#services').scrollIntoView({
-    behavior: 'smooth'
-});}
-
-function skills(){document.querySelector('#skills').scrollIntoView({
-    behavior: 'smooth'
-  });}
-
-function teams(){document.querySelector('#teams').scrollIntoView({
-    behavior: 'smooth'
-});}
+$('.navbar .menu li a').click(function(){
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+});
